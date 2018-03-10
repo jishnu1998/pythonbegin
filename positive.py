bd35461a4313a70db70de76c709404a51481ed19
jishnu1998 <37238435+jishnu1998@users.noreply.github.com>
2018-03-10 15:48:41 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,13 @@
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+num = float(input("Enter a number: "))
+if num > 0:
+ print("Positive number")
+elif num == 0:
+ print("Zero")
+else:
+ print("Negative number")
